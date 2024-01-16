67167cf08165aa00ebeec0da377cc965e7bdabef
wafa1024 <wafa1024@users.noreply.github.com>
2024-01-16 11:06:15 -0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 from os import environ
 SESSION_CONFIG_DEFAULTS = dict(real_world_currency_per_point=1, participation_fee=0)
-SESSION_CONFIGS = [dict(name='my_session', num_demo_participants=20, app_sequence=['prisoner'])]
+SESSION_CONFIGS = [dict(name='my_session', num_demo_participants=2, app_sequence=['prisoner'])]
 LANGUAGE_CODE = 'en'
 REAL_WORLD_CURRENCY_CODE = 'USD'
 USE_POINTS = True
